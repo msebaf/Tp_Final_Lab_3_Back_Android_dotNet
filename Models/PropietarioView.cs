11c90169bda3821aec339f6b4d6f985901d3926e
msebaf <msebaf@hotmail.com>
2023-06-10 04:20:30 -0300

    .

--- Models/PropietarioView.cs
+++ Models/PropietarioView.cs
@@ -13,11 +13,11 @@ public class PropietarioView
         this.Email = propietario.Email;
         this.Nombre = propietario.Nombre;
         this.Apellido = propietario.Apellido;
-        this.Avatar = propietario.Avatar;
+       
         
         this.Dni = propietario.Dni;
         this.Telefono = propietario.Telefono;
-        this.Direccion = propietario.Direccion;
+        
         this.Nacimiento = propietario.Nacimiento;
         
      
@@ -25,10 +25,10 @@ public class PropietarioView
     public int Id { get; set; } 
      public string? Nombre { get; set; }="";
     public string? Apellido { get; set; }="";
-    public string? Avatar { get; set; }="";
+    
     public string? Email { get; set; }="";
   
-    public string Direccion { get; set; }="";
+  
     public DateTime? Nacimiento { get; set; }
     public String? Dni { get; set; }
     public String? Telefono { get; set; }
